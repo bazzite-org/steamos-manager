f90667be64520360bebc03b5909da86401de6ff4
Antheas Kapenekakis <git@antheas.dev>
2025-11-09 23:52:55 +0100

    rework sync script

--- sync.sh
+++ sync.sh
@@ -3,18 +3,24 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+set -e
+
 HOST=$1
 RSYNC="rsync -rv --exclude .git --exclude venv --exclude __pycache__'"
 USER=${2:-bazzite}
 
 cargo build --release
-$RSYNC target/debug/steamos-manager $HOST:
+$RSYNC target/release/steamos-manager $HOST:
 
 ssh $HOST /bin/bash << EOF
     sudo rpm-ostree usroverlay
     sudo mv steamos-manager /usr/bin/steamos-manager
+    sudo chmod +x /usr/bin/steamos-manager
+    sudo chcon -R -u system_u -r object_r --type=bin_t /usr/bin/steamos-manager
 
-    sudo systemctl restart steamos-manager.service
-    systemctl --user restart steamos-manager.service
+    sudo systemctl stop steamos-manager.service
+    systemctl --user stop steamos-manager.service
+    sudo systemctl start steamos-manager.service
+    systemctl --user start steamos-manager.service
     bazzite-session-select gamescope
 EOF
